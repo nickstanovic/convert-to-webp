d4353d417af52bab172d395320564997e3f6b360
Nick Stanovic <nickstanovic@gmail.com>
2023-03-23 09:45:27 -0400

    Initial commit

--- convert_jpg_to_webp.py
+++ convert_jpg_to_webp.py
@@ -1,50 +1,22 @@
-#!/usr/bin/env python
 import os
-from gimpfu import *
+from PIL import Image
 
-def convert_jpg_to_webp(input_path, output_path, quality=90, lossless=False):
-    image = pdb.gimp_file_load(input_path, input_path)
-    drawable = pdb.gimp_image_get_active_layer(image)
 
-    # Set the WebP save parameters
-    preset = 4  # Default preset
-    alpha_quality = 0
-    animation = False
-    minimize_size = False
-    keyframe_distance = 0
-    kmin = 0
-    kmax = 0
-    filter_strength = 0
+def convert_jpg_to_webp(input_path, output_path):
+    with Image.open(input_path) as img:
+        img.save(output_path, 'webp', quality=60, lossless=False)
 
-    # Save as WebP
-    pdb.file_webp_save(
-        image,
-        drawable,
-        output_path,
-        output_path,
-        quality,
-        alpha_quality,
-        lossless,
-        animation,
-        preset,
-        keyframe_distance,
-        kmin,
-        kmax,
-        filter_strength
-    )
-
-    # Clean up
-    pdb.gimp_image_delete(image)
 
 def main():
-    current_directory = os.getcwd()
+    input_path = "C:\\Users\\nicks\\Downloads\\"
+    output_path = "C:\\Users\\nicks\\Downloads\\webp\\"
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    for file in os.listdir(input_path):
+        if file.endswith(".jpg"):
+            filename = os.path.splitext(file)[0] + ".webp"
+            convert_jpg_to_webp(input_path + file, output_path + filename)
 
-    for filename in os.listdir(current_directory):
-        if filename.lower().endswith(".jpg"):
-            input_path = os.path.join(current_directory, filename)
-            output_path = os.path.join(current_directory, os.path.splitext(filename)[0] + ".webp")
-            convert_jpg_to_webp(input_path, output_path)
 
 if __name__ == "__main__":
     main()
-
