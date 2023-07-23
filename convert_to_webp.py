0fb32638f075f2e9ba96a6c745e79ac854907b4f
Nick Stanovic <nickstanovic@gmail.com>
2023-07-23 00:26:01 -0400

    add PNG conversion and refactor names

--- convert_to_webp.py
+++ convert_to_webp.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 
 
-def convert_jpg_to_webp(input_path, output_path):
+def convert_to_webp(input_path, output_path):
     with Image.open(input_path) as img:
         img.save(output_path, 'webp', quality=60, lossless=False)
 
@@ -13,9 +13,9 @@ def main():
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     for file in os.listdir(input_path):
-        if file.endswith(".jpg"):
+        if file.endswith(".jpg") or file.endswith(".png"):
             filename = os.path.splitext(file)[0] + ".webp"
-            convert_jpg_to_webp(input_path + file, output_path + filename)
+            convert_to_webp(input_path + file, output_path + filename)
 
 
 if __name__ == "__main__":
